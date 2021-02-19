a472dacbd01330fa2a66f05cb140c584a7ec4ce2
aarushthechamp14 <75982276+aarushthechamp14@users.noreply.github.com>
2021-02-19 17:03:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var database;
+var canvas,backgroundImg;
+var gameState = 0
+var playerCount;
+var form,player,game;
+
+function setup(){
+   canvas = createCanvas(500,500);
+   database = firebase.database()
+
+   game = new Game()
+   game.getState()
+   game.start();
+   
+}
+
+function draw(){
+    
+}
